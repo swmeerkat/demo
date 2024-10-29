a69e792850165d54af8d811b279f125a0d2b1bd4
Martin <martin.takats@t-online.de>
2024-10-29 09:41:40 +0100

    jacoco

--- src/test/java/com/example/demo/integration/FibonacciControllerIT.java
+++ src/test/java/com/example/demo/integration/FibonacciControllerIT.java
@@ -1,23 +1,16 @@
 package com.example.demo.integration;
 
-import com.example.demo.fibonacci.FibonacciResult;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
-import java.util.Objects;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static io.restassured.RestAssured.when;
 
 public class FibonacciControllerIT extends BaseIT {
 
     @Test
-    public void getFibonacciSequence() {
-        ResponseEntity<FibonacciResult> response =
-                this.restTemplate.getForEntity("http://localhost:" + port + "/fibonacci/0", FibonacciResult.class);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertNotNull(Objects.requireNonNull(response.getBody()).getSequence());
-        assertEquals(50, response.getBody().getSequence().length);
+    public void getFibonacciSequenceRA() {
+        when()
+                .request("GET", "http://localhost:" + port + "/fibonacci/50")
+                .then()
+                .statusCode(200);
     }
 }
